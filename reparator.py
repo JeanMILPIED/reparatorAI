a1eec77e5e663fa1ce545ea38f3c1b8e44409974
JEAN <56645415+JeanMILPIED@users.noreply.github.com>
2022-05-09 07:53:33 +0200

    Update reparator.py

--- reparator.py
+++ reparator.py
@@ -3,6 +3,10 @@ import pandas as pd
 import numpy as np
 from datetime import datetime
 import csv
+import pathlib
+from bs4 import BeautifulSoup
+import logging
+import shutil
 
 col10, col11=st.columns(2)
 col11.title('REPARATOR.AI ')
@@ -129,6 +133,42 @@ if st.button("let's find repairs! 🧠 "):
 
 st.caption('data source is : https://openrepair.org/open-data/downloads/')
 st.caption('you want to contribute ? I am a huge coffee fan! https://www.buymeacoffee.com/jeanmilpied ')
+
+#insert the google anlaytics
+
+def inject_ga():
+    GA_ID = "google_analytics"
+
+    # Note: Please replace the id from G-XXXXXXXXXX to whatever your
+    # web application's id is. You will find this in your Google Analytics account
+    
+    GA_JS = """
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-MGD4ES78W6"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-MGD4ES78W6');
+    </script>
+    """
+
+    # Insert the script in the head tag of the static template inside your virtual
+    index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
+    logging.info(f'editing {index_path}')
+    soup = BeautifulSoup(index_path.read_text(), features="html.parser")
+    if not soup.find(id=GA_ID):  # if cannot find tag
+        bck_index = index_path.with_suffix('.bck')
+        if bck_index.exists():
+            shutil.copy(bck_index, index_path)  # recover from backup
+        else:
+            shutil.copy(index_path, bck_index)  # keep a backup
+        html = str(soup)
+        new_html = html.replace('<head>', '<head>\n' + GA_JS)
+        index_path.write_text(new_html)
+
+
+inject_ga()
 
 
 

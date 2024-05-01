120ed366db6d9a742f2e42287194e2be8f03beca
JEAN <jean.milpied@gmail.com>
2024-05-01 16:03:53 +0200

    ca marche

--- probe-action/probe.js
+++ probe-action/probe.js
@@ -3,13 +3,22 @@ const TARGET_URL = "https://reparatorai.streamlit.app/";
 const WAKE_UP_BUTTON_TEXT = "app back up";
 const PAGE_LOAD_GRACE_PERIOD_MS = 8000;
 
+console.log(process.version);
+
 (async () => {
-    const browser = await puppeteer.launch(
-        { args: ["--no-sandbox"] }
-    );
+     const browser = await puppeteer.launch({
+        headless:true,
+        ignoreHTTPSErrors:true,
+        args: ['--no-sandbox']
+  });
 
     const page = await browser.newPage();
+    console.log(page); // Print the page object to inspect its properties
+
     await page.goto(TARGET_URL);
+
+    console.log(page); // Print the page object to inspect its properties
+
     // Wait a grace period for the application to load
     await page.waitForTimeout(PAGE_LOAD_GRACE_PERIOD_MS);
 
